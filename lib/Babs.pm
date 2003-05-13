2a002121d32ac5aa19dc042591ba0cced5df684b
jaredy <>
2003-05-13 22:13:36 +0000

    added Babs::throw() instead of instance->{wasp}->throw, added code to initialize OF and DBH, babs-config now uses a typeglob to refer to the type at hand, and started initial mod_perl/req obj support

--- lib/Babs.pm
+++ lib/Babs.pm
@@ -6,6 +6,16 @@ use OF;
 use DBH qw(:all);
 use Exporter;
 
+BEGIN: {
+	if ($ENV{MOD_PERL})
+	{
+		require Apache;
+		require Apache::Request;
+	} else {
+		require CGI;
+	}
+}
+
 use strict;
 use constant TRUE => 1;
 use constant FALSE => 0;
@@ -19,20 +29,51 @@ sub new
 {
 	my ($class, %prefs) = @_;
 
-	my $this = bless {
-		skip_init	=> exists $prefs{skip_init} ? $prefs{skip_init} : FALSE,
-		wasp		=> $prefs{wasp},
-		of		=> $prefs{of},
-	}, ref($class) || $class;
-
+	# This should fill up %prefs
 	require "babs-config.inc";
 
-	$this->udf_update() unless $this->{skip_init};
+	my $this = bless \%prefs, ref($class) || $class;
+	
+	# Initialize DBH
+	{
+		my %args = (wasp=>$this->{wasp});
+		my %map = (
+			host		=> "dbh_host",
+			username	=> "dbh_username",
+			password	=> "dbh_password",
+			database	=> "dbh_database",
+		);
+		# These settings are optional; only pass them
+		# to DBH::new() if they are specified.
+		my ($k, $v);
+		while (($k, $v) = each %map)
+		{
+			$args{$k} = $this->{$v} if exists $this->{$v};
+		}
+		$this->{dbh} = &{$this->{dbh_type}}(*{$this->{dbh_type}}{PACKAGE},
+			%args);
+	}
+
+	# Initialize OF
+	{
+		# Load OF prefs
+		my %of_prefs = ();
+		$this->{of} = &{$this->{of_type}}(*{$this->{of_type}}{PACKAGE},
+			$this->{wasp}, \%of_prefs);
+	}
 
 	# Propagate construction
 	$this->_xml_init();
+	$this->_udf_init();
 
 	return $this;
+}
+
+sub throw
+{
+	my ($this) = shift;
+	my $msg = join '', @_;
+	$this->{wasp}->throw("Babs error: $msg");
 }
 
 require "comments.inc";
