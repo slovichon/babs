c14ee4aaff91854613be49f5794dbef07397700d
jaredy <>
2003-06-25 13:35:46 +0000

    more initial work

--- tests/suite.pl
+++ tests/suite.pl
@@ -2,6 +2,16 @@
 
 use strict;
 use Babs;
+use WASP;
+use Thraxx;
+use OF::HTML;
 
+my $w = WASP->new;
+$w->display(1);
+my $t = Thraxx->construct(wasp=>$w, dbh=>1, skip_init=>1);
+my $b = Babs->construct(thraxx=>$t, wasp=>$t, dbh=>undef, skip_init=>1,
+			isapi=>CGI->new, of=>OF::HTML->new($w), dbh=>1);
+
+print Babs::FOOBAR();
 
 exit 0;
