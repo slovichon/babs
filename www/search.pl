480b700ab456b1f7c4b2339f635e216308487c0d
jaredy <>
2003-07-07 12:37:21 +0000

    initial checkin

---
+++ www/search.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -W
+
+use strict;
+use Babs;
+
+my $babs = Babs->new;
+my ($query, $template);
+my ($sort);
+
+$babs->story_search();
+
+if ($query) {
+
+} elsif () {
+} else {
+	# Show search form
+}
+
+exit 0;
