2dffff7b7acaaf7f57f47c02b1d2c0706b456e51
jaredy <>
2003-10-07 21:49:54 +0000

    initial checkin

---
+++ www/admin/story-add.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use Babs;
+use strict;
+use warnings;
+
+
+
+exit 0;
---
+++ www/logout.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use Babs;
+use strict;
+use warnings;
+
+my $babs = Babs->new(shift);
+
+exit 0;
