bf1612a7e839c70b2401ebd23b6a75408de86729
jaredy <>
2003-10-07 21:56:19 +0000

    initial work

--- www/view.pl
+++ www/view.pl
@@ -1,9 +1,23 @@
 #!/usr/bin/perl -W
+# $Id$
 
 use Babs;
 use strict;
 use warnings;
 
 my $babs = Babs->new(shift);
+my $cgi = $babs->{cgi};
 
-if ()
+my $template = $cgi->param("template") || "";
+$template = "story" unless $babs->valid_template("story", $template);
+
+my $story_id = $cgi->param("story_id") || 0;
+$story_id = 0 unless $story_id && $story_id =~ /^\d+$/;
+
+if (my $story = $babs->template_expand($template, {story_id => $story_id})) {
+	print $story;
+} else {
+	$babs->error_page("The requested story could not be found.");
+}
+
+exit 0;
