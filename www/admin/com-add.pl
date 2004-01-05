02f0cea7d0f85f7f18e049cbbc5770702aedf417
jaredy <>
2004-01-05 18:39:29 +0000

    more initial work

--- www/admin/com-add.pl
+++ www/admin/com-add.pl
@@ -6,17 +6,39 @@ use strict;
 use warnings;
 
 my $babs = Babs->new(shift);
-$babs->require_login(Babs::PERM_STORY_ADD);
+my $story_id   = $babs->req("story_id");
+my $comment_id = $babs->req("comment_id");
+my $user_id = $babs->require_login(Babs::PERM_COMMENT_ADD, $story_id, $comment_id);
 
 my $oof = $babs->{oof};
+my $printform = 1;
+my $errmsg = "";
 
 if ($babs->req("submitted")) {
-	# "Add Story" form submitted; add story
+	# "Add Comment" form submitted; try to add comment
+
+	if ($errmsg) {
+		# There was an error; redisplay "Add Comment" form
+		$printform = 0;
+	} else {
+		# No error; try to add the comment
+	}
 }
 
-if () {
-	# Simple request; display "Add Story" form
-	print	$babs->expand_template(),
-		$oof->header("Adding Story"),
-		$babs->expand_template();
+if ($printform) {
+	# Simple request; display "Add Comment" form
+	print	$babs->header("Adding Comment"),
+		$oof->header("Adding Comment"),
+		$oof->form(
+			$oof->table(
+				{class=>"babsTable"},
+				[{class=>"babsDesc", value=>"Title:"},
+				 {class=>$babs->gen_class, value=>
+				  $oof->input(type=>"text", name=>"title", value=>$fields{title})}],
+				[{class=>"babsDesc", value=>":"},
+				 {class=>$babs->gen_class, value=>
+				  $oof->input(type=>"text", name=>"title", value=>$fields{title})}],
+			)
+		),
+		$babs->footer();
 }
