63f725f8d25cb5844f84794a9f9569a1cfaa1370
jaredy <>
2004-01-11 16:04:04 +0000

    more initial testing work

--- tests/suite.pl
+++ tests/suite.pl
@@ -30,7 +30,36 @@ my $d = DBH::MySQL->new(host=>"12.226.98.118", username=>"babs",
 		password=>"", database=>"babs",
 		wasp=>$w);
 my $t = Thraxx->construct(wasp=>$w, dbh=>$td, skip_init=>1);
+
+# We normally invoke Babs->new to let it consult the configuration
+# and set up these objects by itself, but since this test script
+# needs to be run in most any circumstance, we must make it so
+# these settings can be run from anywhere unmodified.
 my $b = Babs->construct(thraxx=>$t, wasp=>$t, dbh=>undef, skip_init=>1,
 			isapi=>CGI->new, of=>OF::HTML->new($w), dbh=>1);
+
+# Some of the operations tested must be done in a specific order,
+# such as adding users before posting stories from them. As this
+# model can leave the system in an unknown state should a procedure
+# fail, cleanups will have to be performed.
+
+# users.inc
+$b->user_add();
+
+# comments.inc
+
+$b->comment
+
+# crypt.inc
+# event.inc
+# isr.inc
+# misc.inc
+# of.inc
+# sessions.inc
+# stories.inc
+# str.inc
+# templates.inc
+# udf.inc
+# xml.inc
 
 exit 0;
