197bf26a96808ba664d53c32c0f11c7c07347648
jaredy <>
2004-03-27 21:46:32 +0000

    description; some initial code; cvsid

--- www/search.pl
+++ www/search.pl
@@ -1,17 +1,36 @@
 #!/usr/bin/perl -W
+# $Id$
 
-use strict;
+# The search and display functionality here is designed to
+# be as flexible as possible, but no more.  Parameters include:
+#
+#	template=$template_name	Name of template to search/display.
+#	param=$val, param=...	Any number of parameters specific to
+#				the search type.
+#	sort=$sort_field	Field to sort on.
+#	order=$sort_order	Ascending or descending.
+#	nresults=$nresults	Number of results to fetch and display.
+#	offset=$page_offset	Page offset.
+#
+# Try to associate search results with the user's session.  There is
+# a good chance the user will peruse a few results at least.  Saving
+# them is at least better than requesting them on each page display.
+
 use Babs;
+use strict;
+use warnings;
 
-my $babs = Babs->new;
-my ($query, $template);
-my ($sort);
+my $babs  = Babs->new(shift);
+my $cgi   = $babs->{cgi};
+my $valid = 1;
 
-$babs->story_search();
+my ($template, @params, $sort, $order, $nresults, $offset);
 
-if ($query) {
+$template = $cgi->param("template");
 
-} elsif () {
+if ($babs->template_exists($template)) {
+	
 } else {
-	# Show search form
 }
+
+EXIT_SUCCESS;
