dec5c8a4fe8888d57fb63aae4e23614ff9c76fb7
jaredy <>
2004-04-03 05:12:29 +0000

    more initial work

--- www/admin/story-add.pl
+++ www/admin/story-add.pl
@@ -7,7 +7,18 @@ use strict;
 use warnings;
 
 my $babs = Babs->new(shift);
-my $thraxx = $babs->{thraxx};
+my $oof = $babs->{oof};
 
+my $user_id = $babs->require_login(Babs::PERM_STORY_ADD);
+my $story_id = $babs->req("story_id");
+
+my $doform = TRUE;
+
+if ($babs->req("submitted")) {
+	
+}
+
+if ($doform) {
+}
 
 EXIT_SUCCESS;
